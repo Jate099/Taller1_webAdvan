19ad6afcbc415556654d996aa5f434f8df8914e1
Jate099 <42478567+Jate099@users.noreply.github.com>
2019-10-23 02:17:09 -0500

    se inicia codigo de curvas

--- src/Components/Planet/Planet.js
+++ src/Components/Planet/Planet.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 
+
 function Planet(props) {
 
   const classes = useStyles();
 
+  const path = (
+    <path
+      d={
+        `M${props.startPoint}
+        Q${props.controlPoint}
+        C${props.endPoint}
+      `}
+
+      fill="none"
+      stroke={props.color1}
+      strokeWidth={5}
+    />
+  );
+
   return (
     <div className={classes.planet}
-      style={{ 
-        height: props.tam, 
-        width: props.tam, 
+      style={{
+        height: props.tam,
+        width: props.tam,
         //background: props.color, 
-        backgroundImage: `linear-gradient(${props.color1}, ${props.color2})`}}>
+        backgroundImage: `linear-gradient(${props.color1}, ${props.color2})`
+      }}>
+
+      <svg viewBox="0 0 200 200" style={{ maxHeight: 400 }}>{path}</svg>
+
     </div>
   );
 }
