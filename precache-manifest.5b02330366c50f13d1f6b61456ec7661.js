b03848bca0b406fbae088e88de12a15ef8157d35
Sebastián Gaviria <sebastiangaviria@outlook.com>
2019-11-07 16:47:57 -0500

    Updates

---
+++ precache-manifest.5b02330366c50f13d1f6b61456ec7661.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d016b49ee82064962a208c5e5d125b",
+    "url": "/Taller1_webAdvan/index.html"
+  },
+  {
+    "revision": "38084aaf3487c625d05e",
+    "url": "/Taller1_webAdvan/static/js/2.c0913c01.chunk.js"
+  },
+  {
+    "revision": "c07edb8b93981ead5e0b",
+    "url": "/Taller1_webAdvan/static/js/main.75dfc32c.chunk.js"
+  },
+  {
+    "revision": "3494b09a6033361d2b06",
+    "url": "/Taller1_webAdvan/static/js/runtime-main.deb08f66.js"
+  }
+]);
